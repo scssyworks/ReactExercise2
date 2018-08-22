60d5e3dbfeaae51c1fd0451644db74f83bf4f24e
Sachin Singh <ssingh259@sapient.com>
2018-08-22 17:35:59 +0530

    Issues list completed

--- components/base/IssueItem.js
+++ components/base/IssueItem.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import StatusIcon from "../icons/StatusIcon";
+import CommentIcon from "../icons/CommentIcon";
 
 class IssueItem extends Component {
     render() {
-        const { username, repository, issueId, title } = this.props;
+        const {
+            username,
+            repository,
+            issueId,
+            title,
+            created,
+            user,
+            iconClass,
+            commentCount
+        } = this.props;
         const routeLink = `/detail/${username}/${repository}/${issueId}`;
         return (
             <div className="issue-item">
                 <Link to={routeLink} className="issue-info">
-                    <div className="issue-title">{title}</div>
+                    <div className="issue-title">
+                        <StatusIcon iconClass={iconClass} />
+                        {title}
+                    </div>
+                    <div className="issue-props">
+                        <span className="issue-id">#{issueId}: </span>
+                        <span className="issue-openedon">Issue opened on: <span>{created}</span></span>
+                        <span className="issue-by">By: <span>{user}</span></span>
+                    </div>
+                    <div className="issue-comments">
+                        <CommentIcon iconClass="icon-comment" />
+                        <span className="issue-commentcount">{commentCount}</span>
+                    </div>
                 </Link>
             </div>
         );
