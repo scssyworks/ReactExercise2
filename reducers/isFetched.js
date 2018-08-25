ac27e81491d66f9489e54bcc28635bb274ab836a
Sachin Singh <ssingh259@sapient.com>
2018-08-25 21:09:06 +0530

    Minor fix

--- reducers/isFetched.js
+++ reducers/isFetched.js
@@ -1,10 +1,10 @@
 import { UPDATE_ISSUES, DISABLE_CONTROLS, UPDATE_ISSUE_DETAILS } from "../src/constants";
 
-function isFetched(state = false, { type, newState }) {
+function isFetched(state = false, { type, response }) {
     switch (type) {
         case UPDATE_ISSUES:
         case UPDATE_ISSUE_DETAILS:
-            return (typeof newState !== "boolean" || newState);
+            return response.success;
         case DISABLE_CONTROLS: return false;
         default: return state;
     }
